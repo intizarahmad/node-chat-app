8de17be505d31790acd01444a7b2bbb241075f14
Initzar Ahmad <intizarahmad@gmail.com>
2018-11-09 10:39:53 +0530

    message utility

--- server/server.js
+++ server/server.js
@@ -8,27 +8,18 @@ app.use(express.static(publicPath));
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
+const { generateMessage } = require("./utils/message");
 io.on('connection', (socket)=>{
   console.log("New connection");
 
-  socket.emit('newMessage', {
-    from:'admin', 
-    text:"welcome to chatapp"
-  })
-  socket.broadcast.emit('newMessage', {
-    from:'admin', 
-    text:"New user join in chatapp"
-  })
+  socket.emit('newMessage', generateMessage('admin', 'welcome to chat app'))
+  socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'))
   socket.on('disconnect',()=>{
     console.log("Clinet disconnected ");
   });
 
   socket.on('createMessage',(message)=>{
-    io.emit('newMessage',{
-      from: message.from, 
-      text: message.text, 
-      createdAt: new Date()
-    });
+    io.emit('newMessage',generateMessage(message.from,message.text));
   });
 });
 
